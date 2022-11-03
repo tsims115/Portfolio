27926402b6872845dff5be0fb62b8c79fe8aa486
Trenton Sims <tsims115@yahoo.com>
2022-11-03 14:43:11 -0500

    Adding changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,54 +1,55 @@
-/*!
-* Start Bootstrap - Grayscale v7.0.5 (https://startbootstrap.com/theme/grayscale)
-* Copyright 2013-2022 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-grayscale/blob/master/LICENSE)
-*/
-//
-// Scripts
-// 
+function submitToAPI(e) {
+    e.preventDefault();
+         var Namere = /[A-Za-z]{1}[A-Za-z]/;
+         if (!Namere.test($("#name-input").val())) {
+                      alert ("Name can not less than 2 char");
+             return;
+         }
+         var mobilere = /[0-9]{10}/;
+         if (!mobilere.test($("#phone-input").val())) {
+             alert ("Please enter valid mobile number");
+             return;
+         }
+         if ($("#email-input").val()=="") {
+             alert ("Please enter your email id");
+             return;
+         }
 
-window.addEventListener('DOMContentLoaded', event => {
+         var reeamil = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,6})?$/;
+         if (!reeamil.test($("#email-input").val())) {
+             alert ("Please enter valid email address");
+             return;
+         }
 
-    // Navbar shrink function
-    var navbarShrink = function () {
-        const navbarCollapsible = document.body.querySelector('#mainNav');
-        if (!navbarCollapsible) {
-            return;
-        }
-        if (window.scrollY === 0) {
-            navbarCollapsible.classList.remove('navbar-shrink')
-        } else {
-            navbarCollapsible.classList.add('navbar-shrink')
-        }
+    var name = $("#name-input").val();
+    var phone = $("#phone-input").val();
+    var email = $("#email-input").val();
+    var desc = $("#description-input").val();
+    var data = {
+       name : name,
+       phone : phone,
+       email : email,
+       desc : desc
+     };
 
-    };
+    $.ajax({
+      type: "POST",
+      url : "https://vu8bvqdxyh.execute-api.us-east-1.amazonaws.com/alpha/contact-us",
+      dataType: "json",
+      crossDomain: "true",
+      contentType: "application/json; charset=utf-8",
+      data: JSON.stringify(data),
 
-    // Shrink the navbar 
-    navbarShrink();
-
-    // Shrink the navbar when page is scrolled
-    document.addEventListener('scroll', navbarShrink);
-
-    // Activate Bootstrap scrollspy on the main nav element
-    const mainNav = document.body.querySelector('#mainNav');
-    if (mainNav) {
-        new bootstrap.ScrollSpy(document.body, {
-            target: '#mainNav',
-            offset: 74,
-        });
-    };
-
-    // Collapse responsive navbar when toggler is visible
-    const navbarToggler = document.body.querySelector('.navbar-toggler');
-    const responsiveNavItems = [].slice.call(
-        document.querySelectorAll('#navbarResponsive .nav-link')
-    );
-    responsiveNavItems.map(function (responsiveNavItem) {
-        responsiveNavItem.addEventListener('click', () => {
-            if (window.getComputedStyle(navbarToggler).display !== 'none') {
-                navbarToggler.click();
-            }
-        });
-    });
-
-});
+      
+      success: function (res) {
+        // clear form and show a success message
+        alert("Successfull");
+        document.getElementById("contact-form").reset();
+        location.reload();
+        console.log(res);
+      },
+      error: function () {
+        // show an error message
+        alert("UnSuccessfull");
+      }});
+  }
